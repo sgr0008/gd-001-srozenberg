4f95bbe7cb40ca84c971c11e5a085930223972ac
azureA08 <91709926+azureA08@users.noreply.github.com>
2024-10-25 17:51:42 -0300

    Add files via upload

---
+++ src/main/java/com/desa/documentador/backend/repositories/InfoRepository.java
@@ -0,0 +1,17 @@
+package com.desa.documentador.backend.repositories;
+
+import com.desa.documentador.dominio.model.Info;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InfoRepository extends JpaRepository<Info, Long> {
+
+    List<Info> findAll();
+    List<Info> findByNombre(String nombre);
+    //List<Info> findByFechaCreacion(int fechaCreacion);
+    Info findById(int id);
+
+}
