2e99375abb0b0dc3350971da5050106bfdd948a1
sgr0008 <trabajo.8080@gmail.com>
2024-11-01 00:06:06 -0300

    Update info de las clases

--- src/main/java/com/documentador/backend/service/ServiceDocumentadorImpl.java
+++ src/main/java/com/documentador/backend/service/ServiceDocumentadorImpl.java
@@ -1,4 +1,10 @@
 package com.documentador.backend.service;
 
+import org.springframework.stereotype.Service;
+
+/***
+ *  Implementa la logica negocio
+ */
+@Service
 public class ServiceDocumentadorImpl {
 }
--- src/main/java/com/documentador/casodeuso/util/Utiles.java
+++ src/main/java/com/documentador/casodeuso/util/Utiles.java
@@ -3,8 +3,13 @@ package com.documentador.casodeuso.util;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/***
+ * Implementacion de utiles / funciones de formateo de datos en general.
+ */
 public class Utiles {
-
+    /***
+     * @return Fecha actul como int ,formato yyyyMMDD
+     */
     public Integer getFechaActual () {
         long ts = System.currentTimeMillis() / 1000;
         Date currentDate = new Date(ts);
