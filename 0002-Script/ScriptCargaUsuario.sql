36d4d1ac2159942cf4fc083081185a27de452acb
sgr0008 <trabajo.8080@gmail.com>
2024-11-07 18:48:48 -0300

    Update 202411

--- 0002-Script/ScriptCargaUsuario.sql
+++ 0002-Script/ScriptCargaUsuario.sql
@@ -1,51 +1,39 @@
--- ALTA USUARIOs Para probar listado-filtrado-borrado
+-- ALTA USUARIOs Para probar
 INSERT INTO `documentador`.`usuario`
 (`fecha_creacion`,
-`id`,
+`name`)
+VALUES
+(20241104,
+'Prueba10');
+INSERT INTO `documentador`.`usuario`
+(`fecha_creacion`,
 `name`)
 VALUES
 (20241103,
-2,
-'Prueba1');
+'Prueba20');
 INSERT INTO `documentador`.`usuario`
 (`fecha_creacion`,
-`id`,
 `name`)
 VALUES
 (20241103,
-3,
-'Prueba2');
+'Prueba22');
 INSERT INTO `documentador`.`usuario`
 (`fecha_creacion`,
-`id`,
 `name`)
 VALUES
 (20241103,
-4,
-'Prueba2');
-INSERT INTO `documentador`.`usuario`
-(`fecha_creacion`,
-`id`,
-`name`)
-VALUES
-(20241103,
-5,
 'Prueba4');
 INSERT INTO `documentador`.`usuario`
 (`fecha_creacion`,
-`id`,
 `name`)
 VALUES
 (20241103,
-6,
 'Prueba5');
 INSERT INTO `documentador`.`usuario`
 (`fecha_creacion`,
-`id`,
 `name`)
 VALUES
 (20241103,
-7,
 'Prueba6');
 -- CONSULTA USURIOS
 SELECT * FROM documentador.usuario;
