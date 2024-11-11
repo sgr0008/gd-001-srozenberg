11cd4487a3495e95917151dcb24b56a080792e23
sgr0008 <trabajo.8080@gmail.com>
2024-11-10 21:37:54 -0300

    Csaos de prueba de Info

---
+++ 0002-Script/ScriptCargaUsuario.sql
@@ -0,0 +1,39 @@
+-- ALTA users Para probar
+INSERT INTO `documentador`.`user`
+(`fecha_creacion`,
+`name`)
+VALUES
+(20241104,
+'Prueba10');
+INSERT INTO `documentador`.`user`
+(`fecha_creacion`,
+`name`)
+VALUES
+(20241103,
+'Prueba20');
+INSERT INTO `documentador`.`user`
+(`fecha_creacion`,
+`name`)
+VALUES
+(20241103,
+'Prueba22');
+INSERT INTO `documentador`.`user`
+(`fecha_creacion`,
+`name`)
+VALUES
+(20241103,
+'Prueba4');
+INSERT INTO `documentador`.`user`
+(`fecha_creacion`,
+`name`)
+INSERT INTO `documentador`.`user`
+(20241103,
+'Prueba5');
+INSERT INTO `documentador`.`user`
+(`fecha_creacion`,
+`name`)
+VALUES
+(20241103,
+'Prueba6');
+-- CONSULTA USURIOS
+SELECT * FROM documentador.user;
